4f251bce08f39ac4cc036e1233c5be268d92268e
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2023-06-12 10:24:14 +0300

    Create SIGFOX_JSON.js

---
+++ SIGFOX_JSON.js
@@ -0,0 +1,37 @@
+
+
+    "TransactionID": 1926,
+
+    "EventList": [
+
+        {
+
+            "Timestamp": "1685102755",
+
+            "TimeFormat": "Epoch",
+
+            "DataType": "Temperature",
+
+            "Data": "33"
+
+            
+
+        },
+
+        {
+
+            "Timestamp": "1685102755",
+
+            "TimeFormat": "Epoch",
+
+            "DataType": "Humidity",
+
+            "Data": "33.54"
+
+            
+
+        }
+
+    ]
+
+}
